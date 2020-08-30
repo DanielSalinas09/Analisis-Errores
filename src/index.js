89bf500c97ff897c64685753257957b40da11625
unknown <majiar16@gmail.com>
2020-08-30 01:47:55 -0500

    maquetacion mainWindow

--- src/index.js
+++ src/index.js
@@ -1,27 +1,21 @@
+const { app, BrowserWindow } = require('electron');
+const url = require('url');
+const path = require('path');
 
-const {app, BrowserWindow}= require('electron');
-const url= require('url');
-const path= require('path');
+if (process.env.NODE_ENV !== 'prodution')
+    require('electron-reload')(__dirname, {
 
-if(process.env.NODE_ENV!=='prodution')
-require('electron-reload')(__dirname,{
-
-})
+    })
 let mainWindow
-app.on('ready',()=>{
+app.on('ready', () => {
 
-    mainWindow= new BrowserWindow({
-        height:550,
-        width:850
-    }),
-    mainWindow.loadURL(url.format({
-        pathname: path.join(__dirname,'views/principalPage.html'),
-        protocol: 'file',
-        
-    }))
+    mainWindow = new BrowserWindow({
+            height: 550,
+            width: 850
+        }),
+        mainWindow.loadURL(url.format({
+            pathname: path.join(__dirname, 'views/principalPage.html'),
+            protocol: 'file',
+
+        }))
 });
-
-
-
-
-
