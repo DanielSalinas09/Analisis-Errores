8c36a00e4be53202a83b65b51c6aeb0d9326984c
Daniel <danielsalinas70sa7@gmail.com>
2020-08-29 23:28:20 -0500

    primer commit

---
+++ src/index.js
@@ -0,0 +1,27 @@
+
+const {app, BrowserWindow}= require('electron');
+const url= require('url');
+const path= require('path');
+
+if(process.env.NODE_ENV!=='prodution')
+require('electron-reload')(__dirname,{
+
+})
+let mainWindow
+app.on('ready',()=>{
+
+    mainWindow= new BrowserWindow({
+        height:550,
+        width:850
+    }),
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname,'views/principalPage.html'),
+        protocol: 'file',
+        
+    }))
+});
+
+
+
+
+
